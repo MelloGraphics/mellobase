2362d02199a0759830112f0aa21b6a1c7bf308b0
Ashley Pickering <ash@bjmdigital.com.au>
2025-04-26 21:13:09 +1000

    remove swiper block, moved to plugin

--- blocks/swiper/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-i18n'), 'version' => '684a9f46b93238976ff8');
--- blocks/swiper/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '64622d19fc8b7997e11f');
