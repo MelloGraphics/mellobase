5c49d50765ab66c517fbdcc83eee8c726dd7ae6b
Ashley Pickering <ash@bjmdigital.com.au>
2025-08-07 15:23:02 +1000

    add missing scss core blocks to the ignore folder ready for build

---
+++ patterns/submenu-columns.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Title: Submenu with Columns
+ * Slug: mellobase/submenu-with-columns
+ * Description: Initial layout for a submenu with columns
+ * Categories: Navigation, Columns
+ * Keywords: Navigation, Columns, Submenu
+ * Block Types: core/navigation-link, core/group, core/columns
+ */
+?>
+
+<!-- wp:navigation-submenu {"label":"Services","description":"Civil construction for car parks, commercial precincts, rail corridors and infrastructure","url":"#","kind":"custom","style":{"layout":{"selfStretch":"fit","flexSize":null}}} -->
+<!-- wp:group {"tagName":"li","lock":{"move":true,"remove":true},"metadata":{"name":"Sub Content Wrapper"},"className":"submenu__content-wrapper","style":{"spacing":{"blockGap":"var:preset|spacing|medium"}},"layout":{"type":"default"}} -->
+<li class="wp-block-group submenu__content-wrapper"><!-- wp:group {"tagName":"ul","layout":{"type":"grid","columnCount":3,"minimumColumnWidth":null}} -->
+<ul class="wp-block-group"><!-- wp:navigation-link {"label":"Specialised Asphalt","type":"service","description":"Overlays and resurfacing to full-depth asphalt replacement","id":1186,"url":"http://roadsafe-asphalt.local/service/specialised-asphalt/","kind":"post-type","className":"is-style-link-box","style":{"background":{"backgroundImage":{"url":"http://roadsafe-asphalt.local/wp-content/uploads/roadsafe-asphalt-illustration-ashpalt.svg","id":1132,"source":"file","title":"roadsafe-asphalt-illustration-ashpalt"},"backgroundSize":"cover"}}} /-->
+
+<!-- wp:navigation-link {"label":"Patching \u0026amp; Maintenance","type":"service","description":"Urgent callouts, utility reinstatements and ongoing council maintenance ","id":1187,"url":"http://roadsafe-asphalt.local/service/patching-maintenance/","kind":"post-type","className":"is-style-link-box","style":{"background":{"backgroundImage":{"url":"http://roadsafe-asphalt.local/wp-content/uploads/roadsafe-asphalt-illustration-patching.svg","id":1137,"source":"file","title":"roadsafe-asphalt-illustration-patching"},"backgroundSize":"cover"}}} /-->
+
+<!-- wp:navigation-link {"label":"Civil Works","type":"service","description":"Civil construction for car parks, commercial precincts, rail corridors and infrastructure","id":1373,"url":"http://roadsafe-asphalt.local/service/civil-works/","kind":"post-type","className":"is-style-link-box","style":{"background":{"backgroundImage":{"url":"http://roadsafe-asphalt.local/wp-content/uploads/roadsafe-asphalt-illustration-civil-works.svg","id":1133,"source":"file","title":"roadsafe-asphalt-illustration-civil-works"},"backgroundSize":"cover"}}} /-->
+
+<!-- wp:navigation-link {"label":"Line Marking \u0026amp; Crack Sealing","type":"service","description":"Road and platform markings, safety line work and bitumen crack sealing","id":1188,"url":"http://roadsafe-asphalt.local/service/line-marking-crack-sealing/","kind":"post-type","className":"is-style-link-box","style":{"background":{"backgroundImage":{"url":"http://roadsafe-asphalt.local/wp-content/uploads/roadsafe-asphalt-illustration-line-marking.svg","id":1135,"source":"file","title":"roadsafe-asphalt-illustration-line-marking"},"backgroundSize":"cover"}}} /-->
+
+<!-- wp:navigation-link {"label":"Plant \u0026amp; Equipment Hire","type":"service","description":"From profilers to sweepers, we have a large fleet of  equipments and operators for hire","id":1189,"url":"http://roadsafe-asphalt.local/service/plant-equipment-hire/","kind":"post-type","className":"is-style-link-box","style":{"background":{"backgroundImage":{"url":"http://roadsafe-asphalt.local/wp-content/uploads/roadsafe-asphalt-illustration-plant-hire.svg","id":1138,"source":"file","title":"roadsafe-asphalt-illustration-plant-hire"},"backgroundSize":"cover"}}} /-->
+
+<!-- wp:navigation-link {"label":"Browse all services","description":"Learn about all of our available services and operational processes","url":"/services/","kind":"custom","className":"is-style-link-box","style":{"background":{"backgroundImage":{"url":"http://roadsafe-asphalt.local/wp-content/uploads/roadsafe-asphalt-illustration-diagonal.svg","id":1134,"source":"file","title":"roadsafe-asphalt-illustration-diagonal"},"backgroundSize":"cover"}}} /--></ul>
+<!-- /wp:group --></li>
+<!-- /wp:group -->
+<!-- /wp:navigation-submenu -->
